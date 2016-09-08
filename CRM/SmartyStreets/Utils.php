1e3151786bb1a2ae6e142ec39d7110750a427cbd
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-09-08 12:12:00 -0700

    Abstracted add settings and inject widget functions out of alterContent and buildForm hooks so it can be invoked manually from CMS built forms or other forms that do not trigger the normal hooks

---
+++ CRM/SmartyStreets/Utils.php
@@ -0,0 +1,55 @@
+<?php
+
+
+class CRM_SmartyStreets_Utils {
+
+  /**
+   * Function to add settings object for the liveaddress widget
+   *
+   * @param null $mappings
+   */
+  public static function addSmartyStreetsSettings($mappings = null) {
+    $var = CRM_Core_Config::singleton();
+    $url = $var->extensionsURL;
+    $plugin = $url."com.aghstrategies.smartystreets/liveaddress.jquery.js";
+    $helper = "//d79i1fxsrar4t.cloudfront.net/jquery.liveaddress/2.4/jquery.liveaddress.min.js";
+    /*** Get state Abbreviations ***/
+    $sql = "SELECT abbreviation, name FROM civicrm_state_province WHERE country_id = 1228";
+    $dao = CRM_Core_DAO::executeQuery($sql);
+    $abbrevate = array();
+    while ($dao->fetch()) {
+      $abbrevate[$dao->abbreviation] = $dao->name;
+    }
+    /*** Get state id's' ***/
+    $sql = "SELECT id, name FROM civicrm_state_province WHERE country_id = 1228";
+    $dao = CRM_Core_DAO::executeQuery($sql);
+    $stateIDs = array();
+    while($dao->fetch()){
+      $stateIDs[$dao->id] = $dao->name;
+    }
+    $settings =  CRM_Core_BAO_Setting::getItem("SmartyStreets Settings");
+    foreach ($settings as $setting => $value) {
+      if($value == ""){
+        $settings[$setting] = 0;
+      }
+    }
+
+    CRM_Core_Resources::singleton()->addSetting(array('SmartyStreets' => array(
+      'settings' => $settings,
+      'json_abbreviate' => $abbrevate,
+      'json_states' => $stateIDs,
+      'plugin'=> $plugin,
+      'helper' => $helper,
+    )));
+  }
+
+
+  /**
+   * Add the liveaddress widget to the form.
+   */
+  public static function addSmartyStreetsWidget() {
+    CRM_Core_Resources::singleton()->addScriptUrl('//d79i1fxsrar4t.cloudfront.net/jquery.liveaddress/2.4/jquery.liveaddress.min.js', 33, 'html-header');
+    CRM_Core_Resources::singleton()->addScriptFile('com.aghstrategies.smartystreets', 'smartystreets.js', 10, 'page-footer');
+  }
+
+}
--- smartystreets.php
+++ smartystreets.php
@@ -3,47 +3,14 @@
 require_once 'smartystreets.civix.php';
 
 function smartystreets_civicrm_buildForm($formName, &$form){
-	CRM_Core_Resources::singleton()->addScriptUrl('//d79i1fxsrar4t.cloudfront.net/jquery.liveaddress/2.4/jquery.liveaddress.min.js', 33, 'html-header');
-	CRM_Core_Resources::singleton()->addScriptFile('com.aghstrategies.smartystreets', 'smartystreets.js', 10, 'page-footer');
+  CRM_SmartyStreets_Utils::addSmartyStreetsWidget();
 }
 
 function smartystreets_civicrm_alterContent( &$content, $context, $tplName, &$object ) {
-	$tpl = explode("/", $tplName);
-	if($context = "form" && $tpl[1] != "Report"){
-	   $var = CRM_Core_Config::singleton();
-		 $url = $var->extensionsURL;
-		 $plugin = $url."/com.aghstrategies.smartystreets/liveaddress.jquery.js";
-		 $helper = "//d79i1fxsrar4t.cloudfront.net/jquery.liveaddress/2.4/jquery.liveaddress.min.js";
-		  /*** Get state Abbreviations ***/
-		  $sql = "SELECT abbreviation, name FROM civicrm_state_province WHERE country_id = 1228";
-		  $dao = CRM_Core_DAO::executeQuery($sql);
-		  $abbrevate = array();
-		  while ($dao->fetch()) {
-	      $abbrevate[$dao->abbreviation] = $dao->name; 
-	    }
-		  /*** Get state id's' ***/
-		  $sql = "SELECT id, name FROM civicrm_state_province WHERE country_id = 1228";
-		  $dao = CRM_Core_DAO::executeQuery($sql);
-		  $stateIDs = array();
-		  while($dao->fetch()){
-		    $stateIDs[$dao->id] = $dao->name;
-		  }
-		  $settings =  CRM_Core_BAO_Setting::getItem("SmartyStreets Settings");
-		  foreach ($settings as $setting => $value) {
-		  	if($value == ""){
-		  		$settings[$setting] = 0;
-		  	}
-		  }
-		  CRM_Core_Resources::singleton()->addSetting(array('SmartyStreets' => array(
-		  																											'settings' => $settings,
-		  																											'json_abbreviate' => $abbrevate,
-		  																											'json_states' => $stateIDs,
-		  																											'plugin'=> $plugin,
-		  																											'helper' => $helper,
-		  																										)));
-		 
-
-	   }  
+  $tpl = explode("/", $tplName);
+  if($context = "form" && $tpl[1] != "Report"){
+    CRM_SmartyStreets_Utils::addSmartyStreetsSettings();
+  }
 }
 
 function smartystreets_civicrm_navigationMenu( &$params ) {
@@ -53,11 +20,11 @@ function smartystreets_civicrm_navigationMenu( &$params ) {
     'url'        => 'civicrm/admin/setting/smartystreets',
     'permission' => 'administer CiviCRM',
   ));
- /* _smartystreets_civix_insert_navigation_menu($params, 'Administer/Customize Data and Screens', array(
-    'name'       => 'SmartyStreets List Import/Export',
-    'url'        => 'civicrm/admin/setting/smartystreets_lists',
-    'permission' => 'administer CiviCRM',
-  ));*/
+  /* _smartystreets_civix_insert_navigation_menu($params, 'Administer/Customize Data and Screens', array(
+     'name'       => 'SmartyStreets List Import/Export',
+     'url'        => 'civicrm/admin/setting/smartystreets_lists',
+     'permission' => 'administer CiviCRM',
+   ));*/
 }
 /**
  * Implementation of hook_civicrm_config
@@ -72,7 +39,7 @@ function smartystreets_civicrm_config(&$config) {
  * @param $files array(string)
  */
 function smartystreets_civicrm_xmlMenu(&$files) {
-	$files[] = dirname(__FILE__)."/xml/Menu/smartystreets.xml";
+  $files[] = dirname(__FILE__)."/xml/Menu/smartystreets.xml";
 }
 
 /**
@@ -93,17 +60,17 @@ function smartystreets_civicrm_uninstall() {
  * Implementation of hook_civicrm_enable
  */
 function smartystreets_civicrm_enable() {
-	$invalidMessage = "Address not verified";
-	$ambiguousMessage = "Choose the correct address";
-	CRM_Core_BAO_Setting::setItem(1, "SmartyStreets Settings", "auto_verify");
-	CRM_Core_BAO_Setting::setItem(1, "SmartyStreets Settings", "geolocate");
-	CRM_Core_BAO_Setting::setItem(1, "SmartyStreets Settings", "UI");
-	CRM_Core_BAO_Setting::setItem(0, "SmartyStreets Settings", "debug");
-	CRM_Core_BAO_Setting::setItem("10", "SmartyStreets Settings", "autocomplete");
-	CRM_Core_BAO_Setting::setItem("3", "SmartyStreets Settings", "candidates");
-	CRM_Core_BAO_Setting::setItem($invalidMessage, "SmartyStreets Settings", "invalid");
-	CRM_Core_BAO_Setting::setItem($ambiguousMessage, "SmartyStreets Settings", "ambiguous");
-	$session = CRM_Core_Session::singleton();
+  $invalidMessage = "Address not verified";
+  $ambiguousMessage = "Choose the correct address";
+  CRM_Core_BAO_Setting::setItem(1, "SmartyStreets Settings", "auto_verify");
+  CRM_Core_BAO_Setting::setItem(1, "SmartyStreets Settings", "geolocate");
+  CRM_Core_BAO_Setting::setItem(1, "SmartyStreets Settings", "UI");
+  CRM_Core_BAO_Setting::setItem(0, "SmartyStreets Settings", "debug");
+  CRM_Core_BAO_Setting::setItem("10", "SmartyStreets Settings", "autocomplete");
+  CRM_Core_BAO_Setting::setItem("3", "SmartyStreets Settings", "candidates");
+  CRM_Core_BAO_Setting::setItem($invalidMessage, "SmartyStreets Settings", "invalid");
+  CRM_Core_BAO_Setting::setItem($ambiguousMessage, "SmartyStreets Settings", "ambiguous");
+  $session = CRM_Core_Session::singleton();
   $session->replaceUserContext(CRM_Utils_System::url('civicrm/admin/setting/smartystreets'));
   return _smartystreets_civix_civicrm_enable();
 }
